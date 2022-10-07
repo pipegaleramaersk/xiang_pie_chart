c0e93a08d33c43d939a9e4d07c94d666fbe8ceb8
Pipe Galera <pipegalera@gmail.com>
2022-10-07 14:59:36 +0200

    Added a second graph

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,22 +5,25 @@ import streamlit as st
 
 
 #st.set_page_config(layout="wide")
-st.title('Nested Pie Chart')
+st.title('Nested Donut Chart')
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 
-overall_engagement_selected = st.sidebar.slider("Outer line:", 1, 100, value=75)
-maersk_engagement_selected = st.sidebar.slider("Inner line:", 1, 100, value=50)
+outer_line_selected = st.sidebar.slider("Outer line, first chart:", 1, 100, value=75)
+inner_line_selected = st.sidebar.slider("Inner line, first :", 1, 100, value=50)
 
-width = st.sidebar.slider("plot width", 1, 25, value=10)
-height = st.sidebar.slider("plot height", 1, 25, value=10)
+outer_line_selected_2 = st.sidebar.slider("Outer line, second chart:", 1, 500, value=75)
+inner_line_selected_2 = st.sidebar.slider("Inner line, second chart :", 1, 500, value=50)
+
+width = st.sidebar.slider("Plot width", 1, 25, value=10)
+height = st.sidebar.slider("Plot height", 1, 25, value=10)
 
 number_size = st.sidebar.slider("Numbers size", 0, 100, value=50)
 
-def pie_chart(overall_engagement, maersk_engagement):
+def pie_chart(outer_line, inner_line):
     
-    overall_engagement = overall_engagement/100
-    maersk_engagement = maersk_engagement/100
+    outer_line = outer_line/100
+    inner_line = inner_line/100
 
     fig, ax = plt.subplots(figsize=(width, height))
 
@@ -32,8 +35,8 @@ def pie_chart(overall_engagement, maersk_engagement):
     # Pie charts 
 
     size = 0.3
-    outer_vals = [1-overall_engagement, overall_engagement]
-    inner_vals = [1-maersk_engagement, maersk_engagement]
+    outer_vals = [1-outer_line, outer_line]
+    inner_vals = [1-inner_line, inner_line]
 
     outer_colors = ["#CFCFCF", "#42B0D5", ]
     inner_colors = ["#CFCFCF", "#00243E", ]
@@ -49,29 +52,24 @@ def pie_chart(overall_engagement, maersk_engagement):
     Xstart, Xend = ax.get_xlim()
     Ystart, Yend = ax.get_ylim()
 
-    # Overall engagement Survey Rating
-    ax.text(0, Yend+0.9, overall_engagement, fontsize=number_size, fontweight='light', fontfamily='sansserif',color='black', horizontalalignment='center')
-    #ax.text(3, Yend+1, 'Overall Engagement', fontsize=10, fontweight='ultralight', fontfamily='sansserif',color='black')
-    #ax.text(3, Yend+0.8, 'Survey Rating', fontsize=10, fontweight='ultralight', fontfamily='sansserif',color='black')
-    #ax.hlines(y = Yend+ 0.95, xmin = Xstart+1.6, xmax = Xend+1.6, linewidth=0.5)
+    # Outer figure number
+    ax.text(0, Yend+0.9, outer_line, fontsize=number_size, fontweight='light', fontfamily='sansserif',color='black', horizontalalignment='center')
 
-    # Maersk Overall Average
-    ax.text(0, Yend-1.3, maersk_engagement, fontsize=number_size, fontweight='light', fontfamily='sansserif',color='black', horizontalalignment='center')
-    #ax.text(3, Yend-1.3, 'Maersk Overall Average', fontsize=10, fontweight='ultralight', fontfamily='sansserif',color='black')
-    #ax.hlines(y = Yend-1.25, xmin = Xstart+1.6, xmax = Xend+1.6, linewidth=0.5)
+    # Inner figure number
+    ax.text(0, Yend-1.3, inner_line, fontsize=number_size, fontweight='light', fontfamily='sansserif',color='black', horizontalalignment='center')
 
-    # Target
-    #ax.text(2.8, Yend-2, 'Company Target', fontsize=10, fontweight='ultralight', fontfamily='sansserif',color='black')
-    #ax.text(2.3, Yend-2, 0.75, fontsize=10, fontweight='ultralight', fontfamily='sansserif',color='red')
-    #ax.hlines(y = Yend-1.95, xmin = Xstart+1.9, xmax = Xend+1, linewidth=0.5, color='red')
 
 
     ax.set()
     #fig.tight_layout()
 
-fig = pie_chart(overall_engagement=overall_engagement_selected, maersk_engagement=maersk_engagement_selected)
+fig = pie_chart(outer_line=outer_line_selected, inner_line=inner_line_selected)
+
+fig_2 = pie_chart(outer_line=outer_line_selected_2, inner_line=inner_line_selected_2)
+
 
 st.pyplot(fig)
 
 
-    
+st.pyplot(fig_2)
+
