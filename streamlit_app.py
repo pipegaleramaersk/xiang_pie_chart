cfa1b1210689154961f2c5f2b9cbec69126d9b70
Pipe Galera <pipegalera@gmail.com>
2022-10-07 15:04:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,14 +58,54 @@ def pie_chart(outer_line, inner_line):
     # Inner figure number
     ax.text(0, Yend-1.3, inner_line, fontsize=number_size, fontweight='light', fontfamily='sansserif',color='black', horizontalalignment='center')
 
-
-
     ax.set()
     #fig.tight_layout()
 
 fig = pie_chart(outer_line=outer_line_selected, inner_line=inner_line_selected)
 
-fig_2 = pie_chart(outer_line=outer_line_selected_2, inner_line=inner_line_selected_2)
+
+def pie_chart_2(outer_line, inner_line):
+    
+    outer_line = outer_line/500
+    inner_line = inner_line/500
+
+    fig, ax = plt.subplots(figsize=(width, height))
+
+    # Background 
+    background = "white"
+    fig.patch.set_facecolor(background) # figure background color
+    ax.set_facecolor(background)
+
+    # Pie charts 
+
+    size = 0.3
+    outer_vals = [1-outer_line, outer_line]
+    inner_vals = [1-inner_line, inner_line]
+
+    outer_colors = ["#CFCFCF", "#42B0D5", ]
+    inner_colors = ["#CFCFCF", "#00243E", ]
+
+    ax.pie(outer_vals, radius=2, colors=outer_colors,
+            wedgeprops=dict(width=size, edgecolor='w', linewidth= 0), startangle=270)
+
+    ax.pie(inner_vals, radius=1.8-size, colors=inner_colors,
+            wedgeprops=dict(width=size, edgecolor='w', linewidth= 0), startangle=270)
+
+
+    ### Annotations
+    Xstart, Xend = ax.get_xlim()
+    Ystart, Yend = ax.get_ylim()
+
+    # Outer figure number
+    ax.text(0, Yend+0.9, outer_line, fontsize=number_size, fontweight='light', fontfamily='sansserif',color='black', horizontalalignment='center')
+
+    # Inner figure number
+    ax.text(0, Yend-1.3, inner_line, fontsize=number_size, fontweight='light', fontfamily='sansserif',color='black', horizontalalignment='center')
+
+    ax.set()
+    #fig.tight_layout()
+
+fig_2 = pie_chart_2(outer_line=outer_line_selected_2, inner_line=inner_line_selected_2)
 
 
 st.pyplot(fig)
